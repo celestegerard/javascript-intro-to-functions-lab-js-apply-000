b4257e93615a0ccef0bcefdc29da235b333cbdab
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-29 21:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,3 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!";
-} else if (string ==== string) {
-  return "YES INDEED!";
-} else if (string === "I love you, Grandma.") {
-  return "I love you, too.";
-} else {
-  return ();
-}
